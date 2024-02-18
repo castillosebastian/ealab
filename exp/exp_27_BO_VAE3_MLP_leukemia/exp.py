ceeef61923f80485620ef00a09e3dd35c5600c17
sebacastillo <castilloclaudiosebastian@gmail.com>
2024-02-18 17:07:22 -0300

    update

---
+++ exp/exp_27_BO_VAE3_MLP_leukemia/exp.py
@@ -0,0 +1,263 @@
+# Import statements
+import numpy as np
+import json
+import pandas as pd
+import optuna
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import DataLoader
+from torch import nn, optim
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sdmetrics import load_demo
+from sdmetrics.reports.single_table import QualityReport
+import os
+import inspect
+import sys
+# Method to find the root directory (assuming .git is in the root)
+def find_root_dir():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    while current_dir != os.path.dirname(current_dir):  # To avoid infinite loop
+        if ".git" in os.listdir(current_dir):
+            return current_dir
+        current_dir = os.path.dirname(current_dir)
+    return None  # Or raise an error if the root is not found
+root = find_root_dir()
+sys.path.append(root)
+from src.bo_vae_3L import *
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Parameters----------------------------------------------------------------------------------
+exp_dir = root + "/exp/exp_27_BO_VAE3_MLP_leukemia/"
+dataset_name = 'leukemia'
+class_column = 'CLASS'
+# BO
+n_trials = 100
+param_ranges = {
+    'hiden1': {'low': 300, 'high': 1000},
+    'hiden2': {'low': 200, 'high': 800},
+    'hiden3': {'low': 10, 'high': 600},
+    'latent_dim': {'low': 50, 'high': 200},
+    'lr': {'low': 1e-5, 'high': 1e-3},
+    #'epochs': {'low': 1, 'high': 1}
+    'epochs': {'low': 1000, 'high': 5000}
+}
+n_samples = 100
+# Evaluate
+evaluate = False
+show_quality_figs = True
+# Clasify
+max_iter = 500
+
+# Run-------------------------------------------------------------------------------------------
+# Load and process data
+print('-'*100)
+print(f'Starting data access')
+train_df, test_df, scaler, df_base, class_mapping = load_and_standardize_data_thesis(root, dataset_name, class_column)
+print(f'Data set dimensions: {df_base.shape}')
+cols = df_base.columns
+D_in = train_df.shape[1]
+
+traindata_set = DataBuilder(root, dataset_name, class_column, train=True)
+testdata_set = DataBuilder(root, dataset_name, class_column, train=False)
+print(f'Train data after scale and encode class: {traindata_set.x}')
+trainloader = DataLoader(dataset=traindata_set, batch_size=1024)
+testloader = DataLoader(dataset=testdata_set, batch_size=1024)
+# Optimization phase
+print(f'Starting optimization')
+study = optuna.create_study(direction='minimize')
+study.optimize(lambda trial: objective(trial, trainloader, testloader, param_ranges=param_ranges,device=device), n_trials=n_trials)
+print(f'Optimal hyperparameters: {study.best_params}')
+# Save to a JSON file
+best_params = study.best_params
+with open(exp_dir + 'best_params.json', 'w') as f:
+    json.dump(best_params, f, indent=4)
+print("Best parameters saved to 'best_params.json'")
+
+# Plot optimization history
+opt_history = optuna.visualization.plot_optimization_history(study)
+opt_history.show()
+opt_history.write_image(exp_dir + "opt_history.png")
+# Plot hyperparameter importance
+param_importance = optuna.visualization.plot_param_importances(study)
+param_importance.show()
+param_importance.write_image(exp_dir + "param_importance.png")
+# Plot slice
+slice_plot = optuna.visualization.plot_slice(study)
+slice_plot.show()
+slice_plot.write_image(exp_dir + "slice_plot.png")
+# Plot contour of hyperparameters
+contour_plot = optuna.visualization.plot_contour(study, params=['hiden1', 'hiden2', 'latent_dim', 'lr', 'epochs'])
+contour_plot.show()
+contour_plot.write_image(exp_dir + "contour_plot.png")
+
+# Generation phase------------------------------------------------------------------------------
+print('-'*100)
+print(f'Starting generation')
+model = VAutoencoder(D_in, best_params['hiden1'], best_params['hiden2'], best_params['latent_dim']).float().to(device)
+model.apply(weights_init_uniform_rule)
+optimizer = optim.Adam(model.parameters(), lr=best_params['lr'])
+loss_mse = customLoss()
+
+for epoch in range(1, best_params['epochs'] + 1):
+    train(epoch, model, optimizer, loss_mse, trainloader, device)
+
+torch.save(model, exp_dir + 'vautoencoder_complete.pth')
+#model = torch.load(exp_dir + 'vautoencoder_complete.pth')
+#model.eval()  # Set it to evaluation mode if you're doing inference
+
+with torch.no_grad():
+    mus, logvars = [], []
+    for data in testloader:
+        data = data.float().to(device)
+        recon_batch, mu, logvar = model(data)
+        mus.append(mu)
+        logvars.append(logvar)
+    mu = torch.cat(mus, dim=0)
+    logvar = torch.cat(logvars, dim=0)
+
+# Calculate sigma: a concise way to calculate the standard deviation σ from log-variance
+sigma = torch.exp(logvar / 2)
+# Sample z from q
+q = torch.distributions.Normal(mu.mean(dim=0), sigma.mean(dim=0))
+z = q.rsample(sample_shape=torch.Size([n_samples]))
+# Decode z to generate fake data
+with torch.no_grad():
+    pred = model.decode(z).cpu().numpy()
+pred_data = pred[:, :-1]  
+pred_class = pred[:, -1]  
+scaler = trainloader.dataset.standardizer
+fake_data = scaler.inverse_transform(pred_data)  
+# Reshape pred_class to a 2D array
+pred_class_reshaped = pred_class.reshape(-1, 1)  
+# Correct stacking
+fake_data = np.hstack([fake_data, pred_class_reshaped])  
+
+# Create a DataFrame for the fake data
+# Because the generation phase output target class as float
+# you need to convert the target class from float to integer 
+# and then from value > 0, coerce to 1 as class goes from 1 to 3.
+df_fake = pd.DataFrame(fake_data, columns=cols)
+df_fake[class_column] = np.round(df_fake[class_column]).astype(int)
+df_fake[class_column] = np.where(df_fake[class_column]<1, 0, df_fake[class_column])
+df_fake[class_column] = np.where(df_fake[class_column]>=1, 1, df_fake[class_column])
+df_fake.to_csv( exp_dir + 'syndf.csv', sep=',')
+
+# Evaluation phase--------------------------------------------------------------------------
+if evaluate:
+    print('-'*100)
+    print(f'Starting evaluation')
+    df_base_str = str(df_base.dtypes)
+    #metadata_dict = create_metadata_dict(df_base_str, class_column)
+    metadata_dict = create_dictionary(class_column=class_column,cols=cols)
+
+    # Save the dictionary to a JSON file
+    file_path = exp_dir + 'metadata.json'
+    with open(file_path, 'w') as file:
+        json.dump(metadata_dict, file, indent=4)
+
+    with open(file_path, 'r') as file:
+        metadata_dict = json.load(file)
+
+    my_report = QualityReport()
+    my_report.generate(df_base, df_fake, metadata_dict)
+
+    syn_score = my_report.get_score()
+    syn_score_name = exp_dir + 'syn_score.txt'    
+    with open(syn_score_name, 'w') as f:
+        f.write(str(syn_score))
+    syn_properties = my_report.get_properties()
+    syn_properties_name = exp_dir + 'syn_properties.txt'
+    with open(syn_properties_name, 'w') as f:
+        f.write(str(syn_properties))
+
+    if show_quality_figs:
+        fig_pair_trends = my_report.get_visualization(property_name='Column Pair Trends')
+        fig_pair_trends.write_image(exp_dir+ "pair_trends.pdf")
+
+        fig_shapes = my_report.get_visualization(property_name='Column Shapes')
+        fig_shapes.write_image(exp_dir+ "shapes.pdf")
+
+        if show_quality_figs:
+            fig_pair_trends.show()
+            fig_shapes.show()
+
+        my_report.save(filepath= exp_dir + 'demo_data_quality_report.pkl')
+
+# Clasification phase--------------------------------------------------------------------------
+print('-'*100)
+print(f'Starting Clasification')
+X = df_base.drop(columns=[class_column])  # replace `class_column` with the name of your target column
+y = df_base[class_column]
+
+# Split the original data into train and test sets
+X_train_orig, X_test, y_train_orig, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Prepare the synthetic data (assuming it has the same features as the original data)
+X_train_fake = df_fake.drop(columns=[class_column])
+y_train_fake = df_fake[class_column]
+
+def create_mlp_model():
+    # Define your MLP model
+    # Adjust the parameters according to your needs
+    model = MLPClassifier(hidden_layer_sizes=(500, 200, 100), max_iter=max_iter, alpha=0.001,
+                          solver='adam', verbose=10, random_state=42)
+    return model
+
+
+# Create a pipeline with a scaler and the MLP classifier
+pipeline_orig = Pipeline([
+    ('scaler', StandardScaler()),
+    ('mlp', create_mlp_model())
+])
+
+pipeline_fake = Pipeline([
+    ('scaler', StandardScaler()),
+    ('mlp', create_mlp_model())
+])
+
+
+# Train the model on the original data
+pipeline_orig.fit(X_train_orig, y_train_orig)
+
+# Train the model on the synthetic data
+pipeline_fake.fit(X_train_fake, y_train_fake)
+
+# Evaluate on the test set
+y_pred_orig = pipeline_orig.predict(X_test)
+y_pred_fake = pipeline_fake.predict(X_test)
+
+# Generate classification reports
+report_orig = classification_report(y_test, y_pred_orig)
+report_fake = classification_report(y_test, y_pred_fake)
+accuracy_orig = accuracy_score(y_test, y_pred_orig)
+accuracy_fake = accuracy_score(y_test, y_pred_fake)
+report_orig += "\nAccuracy: {:.4f}".format(accuracy_orig)
+report_fake += "\nAccuracy: {:.4f}".format(accuracy_fake)
+
+# Compare the results
+print("Original Data Model Performance:")
+print(report_orig)
+print(accuracy_orig)
+
+print("\nSynthetic Data Model Performance:")
+print(report_fake)
+print(accuracy_fake)
+
+# Define file paths for saving the reports
+report_orig_path = exp_dir + 'classification_report_original.txt'
+report_fake_path = exp_dir + 'classification_report_synthetic.txt'
+
+# Save the reports to text files
+with open(report_orig_path, 'w') as f:
+    f.write(report_orig)
+
+with open(report_fake_path, 'w') as f:
+    f.write(report_fake)
+
+print("Classification reports saved.")
+
+
---
+++ src/bo_vae_3L.py
@@ -0,0 +1,429 @@
+
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import nn, optim
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sdmetrics import load_demo
+from scipy.io import arff
+
+def load_and_standardize_data(path):
+    # read in from csv
+    df = pd.read_csv(path, sep=',')
+    # replace nan with -99
+    df = df.fillna(-99)
+    df = df.values.reshape(-1, df.shape[1]).astype('float32')
+    # randomly split
+    X_train, X_test = train_test_split(df, test_size=0.3, random_state=42)
+    # standardize values
+    scaler = preprocessing.StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    return X_train, X_test, scaler, df
+
+
+def load_and_standardize_data_thesis(root_dir, dataset_name, class_column):
+
+    if dataset_name == 'leukemia':
+        # File paths for leukemia dataset
+        train_file_path = os.path.join(root_dir, 'data', 'leukemia_train_38x7129.arff')
+        test_file_path = os.path.join(root_dir, 'data', 'leukemia_test_34x7129.arff')
+
+        # Load the data
+        tra, _ = arff.loadarff(train_file_path)
+        tst, _ = arff.loadarff(test_file_path)
+
+        # Convert to pandas DataFrame
+        train_df = pd.DataFrame(tra)
+        test_df = pd.DataFrame(tst)
+
+        # Decode byte strings to strings (necessary for string data in arff files)
+        train_df = train_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+        test_df = test_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+
+        # Initialize label encoder
+        label_encoder = LabelEncoder()
+
+        # Fit label encoder and return encoded labels
+        train_df[class_column] = label_encoder.fit_transform(train_df[class_column])
+        test_df[class_column] = label_encoder.transform(test_df[class_column])
+
+        # Create a mapping dictionary for class labels
+        class_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+
+        # Combine the train and test dataframes
+        df = pd.concat([train_df, test_df], ignore_index=True)
+
+        # Standardize only the feature columns (assuming last column is label)
+        feature_columns = df.columns[df.columns != class_column]
+        #feature_columns = df.columns
+        scaler = StandardScaler()
+        train_df[feature_columns] = scaler.fit_transform(train_df[feature_columns])
+        test_df[feature_columns] = scaler.transform(test_df[feature_columns])
+
+        train_df = train_df.to_numpy()
+        test_df = test_df.to_numpy()
+
+        return train_df, test_df, scaler, df, class_mapping
+    
+    elif dataset_name == 'madelon':
+        # File paths for leukemia dataset
+        train_file_path = os.path.join(root_dir, 'data', 'madelon.trn.arff')
+        test_file_path = os.path.join(root_dir, 'data', 'madelon.tst.arff')
+
+        # Load the data
+        tra, _ = arff.loadarff(train_file_path)
+        tst, _ = arff.loadarff(test_file_path)
+
+        # Convert to pandas DataFrame
+        train_df = pd.DataFrame(tra)
+        test_df = pd.DataFrame(tst)
+
+        # Decode byte strings to strings (necessary for string data in arff files)
+        train_df = train_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+        test_df = test_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+
+        # Initialize label encoder
+        label_encoder = LabelEncoder()
+
+        # Fit label encoder and return encoded labels
+        train_df[class_column] = label_encoder.fit_transform(train_df[class_column])
+        test_df[class_column] = label_encoder.transform(test_df[class_column])
+
+        # Create a mapping dictionary for class labels
+        class_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+
+        # Combine the train and test dataframes
+        df = pd.concat([train_df, test_df], ignore_index=True)
+
+        # Standardize only the feature columns (assuming last column is label)
+        feature_columns = df.columns[df.columns != class_column]
+        #feature_columns = df.columns
+        scaler = StandardScaler()
+        train_df[feature_columns] = scaler.fit_transform(train_df[feature_columns])
+        test_df[feature_columns] = scaler.transform(test_df[feature_columns])
+
+        train_df = train_df.to_numpy()
+        test_df = test_df.to_numpy()
+
+        return train_df, test_df, scaler, df, class_mapping
+    elif dataset_name == 'gisette':
+        # File paths for leukemia dataset
+        train_file_path = os.path.join(root_dir, 'data', 'gisette_train.arff')
+        test_file_path = os.path.join(root_dir, 'data', 'gisette_test.arff')
+
+        # Load the data
+        tra, _ = arff.loadarff(train_file_path)
+        tst, _ = arff.loadarff(test_file_path)
+
+        # Convert to pandas DataFrame
+        train_df = pd.DataFrame(tra)
+        test_df = pd.DataFrame(tst)
+
+        # Decode byte strings to strings (necessary for string data in arff files)
+        train_df = train_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+        test_df = test_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+
+        # Initialize label encoder
+        label_encoder = LabelEncoder()
+
+        # Fit label encoder and return encoded labels
+        train_df[class_column] = label_encoder.fit_transform(train_df[class_column])
+        test_df[class_column] = label_encoder.transform(test_df[class_column])
+
+        # Create a mapping dictionary for class labels
+        class_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+
+        # Combine the train and test dataframes
+        df = pd.concat([train_df, test_df], ignore_index=True)
+
+        # Standardize only the feature columns (assuming last column is label)
+        feature_columns = df.columns[df.columns != class_column]
+        #feature_columns = df.columns
+        scaler = StandardScaler()
+        train_df[feature_columns] = scaler.fit_transform(train_df[feature_columns])
+        test_df[feature_columns] = scaler.transform(test_df[feature_columns])
+
+        train_df = train_df.to_numpy()
+        test_df = test_df.to_numpy()
+
+        return train_df, test_df, scaler, df, class_mapping
+    
+    elif dataset_name == 'gcm':
+        # File paths for leukemia dataset
+        train_file_path = os.path.join(root_dir, 'data', 'GCM_Training.arff')
+        test_file_path = os.path.join(root_dir, 'data', 'GCM_Test.arff')
+
+        # Load the data
+        tra, _ = arff.loadarff(train_file_path)
+        tst, _ = arff.loadarff(test_file_path)
+
+        # Convert to pandas DataFrame
+        train_df = pd.DataFrame(tra)
+        test_df = pd.DataFrame(tst)
+
+        # Decode byte strings to strings (necessary for string data in arff files)
+        train_df = train_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+        test_df = test_df.applymap(lambda x: x.decode() if isinstance(x, bytes) else x)
+
+        # Initialize label encoder
+        label_encoder = LabelEncoder()
+
+        # Fit label encoder and return encoded labels
+        train_df[class_column] = label_encoder.fit_transform(train_df[class_column])
+        test_df[class_column] = label_encoder.transform(test_df[class_column])
+
+        # Create a mapping dictionary for class labels
+        class_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
+
+        # Combine the train and test dataframes
+        df = pd.concat([train_df, test_df], ignore_index=True)
+
+        # Standardize only the feature columns (assuming last column is label)
+        feature_columns = df.columns[df.columns != class_column]
+        #feature_columns = df.columns
+        scaler = StandardScaler()
+        train_df[feature_columns] = scaler.fit_transform(train_df[feature_columns])
+        test_df[feature_columns] = scaler.transform(test_df[feature_columns])
+
+        train_df = train_df.to_numpy()
+        test_df = test_df.to_numpy()
+
+        return train_df, test_df, scaler, df, class_mapping
+
+    else:
+        pass
+
+
+class DataBuilder(Dataset):
+    def __init__(self, root, datasetname, classcolumn,  train=True):
+        self.X_train, self.X_test, self.standardizer, _, _ = load_and_standardize_data_thesis(root_dir=root, dataset_name=datasetname, class_column=classcolumn)
+        if train:
+            self.x = torch.from_numpy(self.X_train)
+            self.len=self.x.shape[0]
+        else:
+            self.x = torch.from_numpy(self.X_test)
+            self.len=self.x.shape[0]
+        del self.X_train
+        del self.X_test
+    def __getitem__(self,index):
+        return self.x[index]
+    def __len__(self):
+        return self.len
+
+class VAutoencoder(nn.Module):
+    def __init__(self,D_in,H=50,H2=12,H3=20,latent_dim=3):
+
+        #Encoder
+        super(VAutoencoder,self).__init__()
+        self.linear1=nn.Linear(D_in,H)
+        self.lin_bn1 = nn.BatchNorm1d(num_features=H)
+        self.linear2 = nn.Linear(H,H2)
+        self.lin_bn2 = nn.BatchNorm1d(num_features=H2)
+        self.linear3 = nn.Linear(H2, H3)
+        self.lin_bn3 = nn.BatchNorm1d(num_features=H3)
+        self.linear4 = nn.Linear(H3,H3)
+        self.lin_bn4 = nn.BatchNorm1d(num_features=H3)
+
+        # Latent vectors mu and sigma
+        self.fc1 = nn.Linear(H3, latent_dim)
+        self.bn1 = nn.BatchNorm1d(num_features=latent_dim)
+        self.fc21 = nn.Linear(latent_dim, latent_dim) # For mu
+        self.fc22 = nn.Linear(latent_dim, latent_dim) # For logvar
+
+        # Sampling vector
+        self.fc3 = nn.Linear(latent_dim, latent_dim)
+        self.fc_bn3 = nn.BatchNorm1d(latent_dim)
+        self.fc4 = nn.Linear(latent_dim, H3)
+        self.fc_bn4 = nn.BatchNorm1d(H3)
+
+        # Decoder        
+        self.linear5=nn.Linear(H3,H3)
+        self.lin_bn5 = nn.BatchNorm1d(num_features=H3)
+        self.linear6=nn.Linear(H3,H2)
+        self.lin_bn6 = nn.BatchNorm1d(num_features=H2)
+        self.linear7=nn.Linear(H2,H)
+        self.lin_bn7 = nn.BatchNorm1d(num_features=H)
+        self.linear8=nn.Linear(H,D_in)
+        self.lin_bn8 = nn.BatchNorm1d(num_features=D_in)
+
+        self.relu = nn.ReLU()
+
+    def encode(self, x):
+        lin1 = self.relu(self.lin_bn1(self.linear1(x)))
+        lin2 = self.relu(self.lin_bn2(self.linear2(lin1)))
+        lin3 = self.relu(self.lin_bn3(self.linear3(lin2)))
+        lin4 = self.relu(self.lin_bn4(self.linear4(lin3)))
+
+        fc1 = F.relu(self.bn1(self.fc1(lin4)))
+
+        r1 = self.fc21(fc1)
+        r2 = self.fc22(fc1)
+
+        return r1, r2
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = logvar.mul(0.5).exp_()
+            eps = Variable(std.data.new(std.size()).normal_())
+            return eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def decode(self, z):
+        fc3 = self.relu(self.fc_bn3(self.fc3(z)))
+        fc4 = self.relu(self.fc_bn4(self.fc4(fc3)))
+
+        lin4 = self.relu(self.lin_bn5(self.linear5(fc4)))
+        lin5 = self.relu(self.lin_bn6(self.linear6(lin4)))
+        lin6 = self.relu(self.lin_bn7(self.linear7(lin5)))
+        return self.lin_bn8(self.linear8(lin6))
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+class customLoss(nn.Module):
+    def __init__(self):
+        super(customLoss, self).__init__()
+        self.mse_loss = nn.MSELoss(reduction="sum")
+
+    def forward(self, x_recon, x, mu, logvar):
+        loss_MSE = self.mse_loss(x_recon, x)
+        loss_KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        return loss_MSE + loss_KLD
+
+def weights_init_uniform_rule(m):
+    classname = m.__class__.__name__
+    # for every Linear layer in a model..
+    if classname.find('Linear') != -1:
+        # get the number of the inputs
+        n = m.in_features
+        y = 1.0/np.sqrt(n)
+        m.weight.data.uniform_(-y, y)
+        m.bias.data.fill_(0)
+
+
+def train(epoch, model, optimizer, loss_mse, trainloader, device):
+    model.train()
+    train_loss = 0
+    for batch_idx, data in enumerate(trainloader):
+        data = data.float().to(device)
+        optimizer.zero_grad()
+        recon_batch, mu, logvar = model(data)
+        loss = loss_mse(recon_batch, data, mu, logvar)
+        loss.backward()
+        train_loss += loss.item()
+        optimizer.step()
+    if epoch % 200 == 0:
+        print('====> Epoch: {} Average training loss: {:.4f}'.format(
+            epoch, train_loss / len(trainloader.dataset)))    
+    return train_loss / len(trainloader.dataset)
+
+def test(epoch, model, loss_mse, testloader, device):
+    model.eval()  # Set the model to evaluation mode
+    test_loss = 0
+    with torch.no_grad():
+        for batch_idx, data in enumerate(testloader):
+            data = data.float().to(device)
+            recon_batch, mu, logvar = model(data)
+            loss = loss_mse(recon_batch, data, mu, logvar)
+            test_loss += loss.item()
+
+    average_test_loss = test_loss / len(testloader.dataset)
+    if epoch % 200 == 0:
+        print('===============> Epoch: {} Average test loss: {:.4f}'.format(epoch, average_test_loss))
+    return average_test_loss
+
+def objective(trial,trainloader,testloader, param_ranges=None, device = 'cpu'):
+    # Suggest values for the hyperparameters based on the dictionary
+    hiden1 = trial.suggest_int('hiden1', param_ranges['hiden1']['low'], param_ranges['hiden1']['high'])
+    hiden2 = trial.suggest_int('hiden2', param_ranges['hiden2']['low'], param_ranges['hiden2']['high'])
+    hiden3 = trial.suggest_int('hiden3', param_ranges['hiden3']['low'], param_ranges['hiden3']['high'])
+    latent_dim = trial.suggest_int('latent_dim', param_ranges['latent_dim']['low'], param_ranges['latent_dim']['high'])
+    lr = trial.suggest_float('lr', param_ranges['lr']['low'], param_ranges['lr']['high'])
+    epochs = trial.suggest_int('epochs', param_ranges['epochs']['low'], param_ranges['epochs']['high'])
+    
+    D_in = trainloader.dataset.x.shape[1]
+
+    # Initialize model, optimizer, and loss function with suggested values
+    model = VAutoencoder(D_in, hiden1, hiden2,hiden3, latent_dim).float().to(device)
+    model.apply(weights_init_uniform_rule)
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    loss_mse = customLoss()
+
+    # Training and validation process
+    best_test_loss = float('inf')
+    epochs_no_improve = 0
+    patience = 10  # Number of epochs to wait for improvement before stopping
+
+    for epoch in range(1, epochs + 1):
+        train(epoch, model, optimizer, loss_mse, trainloader, device)
+        test_loss = test(epoch, model, loss_mse, testloader, device)
+
+        # Check if test loss improved
+        if test_loss < best_test_loss:
+            best_test_loss = test_loss
+            epochs_no_improve = 0  # Reset counter
+        else:
+            epochs_no_improve += 1
+
+        # Early stopping check
+        if epochs_no_improve == patience:
+            print(f"Early stopping triggered at epoch {epoch}: test loss has not improved for {patience} consecutive epochs.")
+            break
+
+
+    # Return the final test loss
+    return test_loss
+
+
+def create_metadata_dict(dtype_str, primary_key):
+    # Initialize the dictionary structure
+    metadata = {
+        "primary_key": primary_key,
+        "columns": {}
+    }
+    # Split the string by newlines and iterate over the lines
+    for line in dtype_str.split('\n'):
+        # Ignore the 'dtype: object' line
+        if line.strip() == 'dtype: object':
+            continue
+
+        # Split each line by spaces and filter out empty strings
+        parts = [part for part in line.split(' ') if part]
+
+        # Check if there are two parts (column name and data type)
+        if len(parts) == 2:
+            column_name, dtype = parts
+
+            # Determine the sdtype based on dtype
+            if dtype in ['int64', 'float64']:
+                sdtype = 'numerical'
+            else:
+                sdtype = 'unknown'  # Placeholder for other data types
+
+            # Populate the metadata dictionary
+            metadata['columns'][column_name] = {"sdtype": sdtype}
+
+    return metadata
+
+def create_dictionary(class_column, cols):
+    template = {
+        "primary_key": class_column,
+        "columns": {}
+    }
+    for col in cols:
+        #if col != class_column:
+        template["columns"][col] = {"sdtype": "numerical"}
+
+    return template
